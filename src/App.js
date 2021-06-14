0723106a53ab6507c51a79ac6622291c0430fa08
SureshKuchana <kuchana123.suresh@gmail.com>
2021-06-14 18:45:02 +0530

    form submit api request and composition

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { render } from "react-dom";
-import Pet from "./components/Pet";
+import SearchParams from "./components/SearchParams";
 import "./style.css";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="dog" breed="Havanese" />
+      {/* <Pet name="Luna" animal="dog" breed="Havanese" />
       <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="cat" breed="Mix" />
+      <Pet name="Doink" animal="cat" breed="Mix" /> */}
+      <SearchParams />
     </div>
   );
 };
